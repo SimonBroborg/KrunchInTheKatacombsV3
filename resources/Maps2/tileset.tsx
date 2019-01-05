517ecb189ffa445f1c92cde23fa57f2f295401d5
Simon <kolasasen@gmail.com>
2019-01-05 14:44:11 +0100

    Added the resource folder.

---
+++ resources/Maps2/tileset.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="70" tileheight="70" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1" type="normalTile">
+  <image width="40" height="40" source="../Sprites/Tiles/rockTile.gif"/>
+ </tile>
+ <tile id="2" type="normalTile">
+  <image width="40" height="40" source="../Sprites/Tiles/normalTile2.png"/>
+ </tile>
+ <tile id="3" type="normalTile">
+  <image width="40" height="40" source="../Sprites/Tiles/normalTile.png"/>
+ </tile>
+ <tile id="5" type="normalTile">
+  <image width="40" height="40" source="../../../../../../../IdeaProjects/KrunchInTheCatacombsV2/resources/Sprites/Tiles/wowtile.png"/>
+ </tile>
+ <tile id="179">
+  <image width="70" height="24" source="../Sprites/Tiles/platformerTiles/Tiles/bridgeLogs.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="25" y="11"/>
+   <object id="2" x="40" y="14" width="15" height="3"/>
+   <object id="3" x="22" y="35"/>
+  </objectgroup>
+ </tile>
+ <tile id="180" type="ladder">
+  <image width="70" height="70" source="../Sprites/Tiles/platformerTiles/Tiles/ladder_mid.png"/>
+ </tile>
+ <tile id="182">
+  <image width="70" height="70" source="../Sprites/Tiles/platformerTiles/Tiles/grass.png"/>
+ </tile>
+ <tile id="183">
+  <image width="70" height="70" source="../Sprites/Tiles/platformerTiles/Tiles/lock_red.png"/>
+ </tile>
+ <tile id="184" type="background">
+  <image width="70" height="70" source="../Sprites/Tiles/platformerTiles/Tiles/fence.png"/>
+ </tile>
+ <tile id="185" type="ladder">
+  <image width="70" height="70" source="../Sprites/Tiles/platformerTiles/Tiles/stoneWall.png"/>
+ </tile>
+</tileset>
